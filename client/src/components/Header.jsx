63c017a20a202aefd797c43fac89d4a4998368c2
Martin Farres <91481417+Tats01@users.noreply.github.com>
2022-04-25 17:06:24 -0300

    Logout function

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,26 @@
-import React, { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useEffect, useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/css/Header.css";
 import DropMenu from "./DropMenu";
+import useAuth from "../hooks/useAuth";
+import AuthContext from "../context/AuthProvider";
 
 const Header = () => {
+    const { auth } = useAuth();
+    const { setAuth } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const logout = async () => {
+        setAuth({});
+        navigate("/Login");
+    };
     const path = useLocation();
     useEffect(() => {
-        document.querySelector(".nav_a.active").classList.remove("active");
+        const activeNav = document.querySelector(".nav_a.active");
+        if (activeNav) {
+            activeNav.classList.remove("active");
+        }
         let links = document.querySelectorAll(".nav_a");
         for (let i = 0; i < 4; i++) {
             if (links[i].getAttribute("href") === path.pathname) {
@@ -30,13 +43,25 @@ const Header = () => {
                     <Link to={"/List"} className="nav_a">
                         List
                     </Link>
-
-                    <Link to={"/Register"} className="nav_a">
-                        Register
-                    </Link>
-                    <Link to={"/Login"} className="nav_a">
-                        Login
-                    </Link>
+                    {auth?.user ? (
+                        <>
+                            <Link to={"/Profile"} className="nav_a">
+                                Profile
+                            </Link>
+                            <div onClick={logout} className="nav_a">
+                                Logout
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <Link to={"/Register"} className="nav_a">
+                                Register
+                            </Link>
+                            <Link to={"/Login"} className="nav_a">
+                                Login
+                            </Link>
+                        </>
+                    )}
                 </div>
                 <div className="nav_right ">
                     <DropMenu></DropMenu>
